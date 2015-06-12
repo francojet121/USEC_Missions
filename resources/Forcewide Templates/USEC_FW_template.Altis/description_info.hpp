3feeb8a8ab4ed41e81a515bdba505ef5babbb084
assaultboy <jromano269962@gmail.com>
2015-06-12 11:37:10 -0400

    Added Altis Forcewide Template

---
+++ resources/Forcewide Templates/USEC_FW_template.Altis/description_info.hpp
@@ -0,0 +1,21 @@
+/*--------------------------------------------------------------------
+  Author: Unity Security Force
+
+  description_info.hpp
+  Part of description.ext - Mission description and information
+--------------------------------------------------------------------*/
+
+// Author
+author = "Unity Security Force";
+
+// Mission name
+briefingName = "USEC Forcewide mm/dd/yy"; // in mission select menu
+onLoadName = "Mssion Name"; // on load screen
+
+// Picture shown
+overviewPicture = "usec_images\USEC\preview_orbat.paa"; // in mission select menu
+loadScreen = "usec_images\USEC\preview_orbat.paa"; // on load screen
+
+// Mission description
+overviewText = "The mission section of the SMEAC"; // in mission select menu
+onLoadMission = "The mission section of the SMEAC"; // on load screen
---
+++ resources/Forcewide Templates/USEC_FW_template.Altis/initPlayerServer.sqf
@@ -0,0 +1,9 @@
+private ["_unit","_didJIP"];
+
+_unit = (_this select 0);
+_didJIP = (_this select 1);
+
+_unit call USEC_fnc_syncRole;
+
+
+//Non USEC-Standard code below here ------------------------------------------
