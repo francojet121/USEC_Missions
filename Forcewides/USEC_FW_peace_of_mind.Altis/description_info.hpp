e2bd1989b01cfa2f5ef94148bddd48815ea5f068
assaultboy <jromano269962@gmail.com>
2015-06-11 23:41:19 -0400

    Fixed 6/13/15 Forcewide
    Somehow not all files were copied to the new mission folder. Adding them
    with this commit

---
+++ Forcewides/USEC_FW_peace_of_mind.Altis/description_info.hpp
@@ -0,0 +1,21 @@
+/*--------------------------------------------------------------------
+  Author: Unity Security Force
+
+  description_info.hpp
+  Part of description.ext - Mission description and information
+--------------------------------------------------------------------*/
+
+// Author
+author = "John Romano";
+
+// Mission name
+briefingName = "USEC Forcewide 6/13/15"; // in mission select menu
+onLoadName = "Peace of Mind"; // on load screen
+
+// Picture shown
+overviewPicture = "usec_images\USEC\preview_orbat.paa"; // in mission select menu
+loadScreen = "usec_images\USEC\preview_orbat.paa"; // on load screen
+
+// Mission description
+overviewText = "USEC forces must eliminate all insurgent presence in the town of Rodopoli"; // in mission select menu
+onLoadMission = "USEC forces must eliminate all insurgent presence in the town of Rodopoli"; // on load screen
---
+++ Forcewides/USEC_FW_peace_of_mind.Altis/onPlayerRespawn.sqf
@@ -0,0 +1,14 @@
+private ["_newUnit","_oldUnit"];
+
+_newUnit = (_this select 0);
+_oldUnit = (_this select 1);
+
+//Loadout the player only if not the initial respawn
+if (!(_oldUnit isEqualTo objNull)) then {
+	[_newUnit] call USEC_fnc_giveLoadout;
+};
+
+//Sync the players rank with his name
+_newUnit call USEC_fnc_syncRank;
+
+//Non USEC-Standard code below here ------------------------------------------
