2e9f91b01a03e49c93579a2d7efe9840a68f161c
assaultboy <jromano269962@gmail.com>
2015-09-06 14:14:31 -0400

    Renamed training -> blank and uploaded newest version

---
+++ Standalone/Blanks/USEC_Blank.Altis/description_info.hpp
@@ -0,0 +1,21 @@
+/*--------------------------------------------------------------------
+  Author: Unity Security Force
+
+  description_info.hpp
+  Part of description.ext - Mission description and information
+--------------------------------------------------------------------*/
+
+// Author
+author = "Unity Security Force";
+
+// Mission name
+briefingName = "USEC Blank Mission"; // in mission select menu
+onLoadName = "USEC Blank Mission"; // on load screen
+
+// Picture shown
+overviewPicture = "usec_images\Orbats\orbat_blank_altis.paa"; // in mission select menu
+loadScreen = "usec_images\Orbats\orbat_blank_altis.paa"; // on load screen
+
+// Mission description
+overviewText = "Create your own missions and have some fun"; // in mission select menu
+onLoadMission = "Create your own missions and have some fun"; // on load screen
