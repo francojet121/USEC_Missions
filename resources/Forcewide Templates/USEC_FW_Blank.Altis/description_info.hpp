f7dc055b637d4f291585affc8482f3b4ced9d912
assaultboy <jromano269962@gmail.com>
2015-09-13 19:41:38 -0400

    Updated Forcewide Template

---
+++ resources/Forcewide Templates/USEC_FW_Blank.Altis/description_info.hpp
@@ -0,0 +1,21 @@
+/*--------------------------------------------------------------------
+  Author: Unity Security Force
+
+  description_info.hpp
+  Part of description.ext - Mission description and information
+--------------------------------------------------------------------*/
+
+// Author
+author = "Unity Security Force";
+
+// Mission name
+briefingName = "USEC Blank Forcewide"; // in mission select menu
+onLoadName = "USEC Blank Forcewide"; // on load screen
+
+// Picture shown
+overviewPicture = "usec_images\Orbats\orbat_blank_altis.paa"; // in mission select menu
+loadScreen = "usec_images\Orbats\orbat_blank_altis.paa"; // on load screen
+
+// Mission description
+overviewText = "For those situations when the question is raised Saturday morning"; // in mission select menu
+onLoadMission = "For those situations when the question is raised Saturday morning"; // on load screen
--- resources/Forcewide Templates/USEC_FW_Blank.Altis/onPlayerRespawn.sqf
+++ resources/Forcewide Templates/USEC_FW_Blank.Altis/onPlayerRespawn.sqf
@@ -3,11 +3,6 @@ private ["_newUnit","_oldUnit"];
 _newUnit = (_this select 0);
 _oldUnit = (_this select 1);
 
-//Loadout the player only if not the initial respawn
-if (!(_oldUnit isEqualTo objNull)) then {
-	[_newUnit] call USEC_fnc_giveLoadout;
-};
-
 //Sync the players rank with his name
 _newUnit call USEC_fnc_syncRank;
 
